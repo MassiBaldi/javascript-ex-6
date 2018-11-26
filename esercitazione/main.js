9b55c17241e415130fbe13ab847abcb1959229de
MassiBaldi <massibaldi85@gmail.com>
2018-11-26 21:55:39 +0100

    Finito e funziona

--- esercitazione/main.js
+++ esercitazione/main.js
@@ -3,3 +3,24 @@
 // Sulla pagina html deve apparire “Ciao <nome>”, il
 // colore del nome deve essere azzurro o rosa a
 // seconda del sesso inserito
+
+//chiedo il nome all'utente
+var nomeUtente = prompt('Scrivi il tuo nome');
+//modifico html col nome utente
+var span_NUtente = document.getElementById('utente');
+span_NUtente.innerHTML = nomeUtente;
+//chiedo il sesso all'nomeUtente
+var sessoUtente = prompt('Scrivi il tuo sesso (maschio o femmina)');
+sessoUtente = sessoUtente.toLowerCase();
+//se è maschio il nome diventa azzurro
+if (sessoUtente == 'maschio') {
+  span_NUtente.style.color = '#2572ed';
+}
+//se è femmina il nome diventa rosa
+else if (sessoUtente == 'femmina') {
+  span_NUtente.style.color = '#f49a9a';
+}
+//se si sbaglia a scrivere il sesso viene richiesto
+else {
+  var sessoSbagliato = prompt('Hai sbagliato a scrivere il sesso... Riprova.');
+}
